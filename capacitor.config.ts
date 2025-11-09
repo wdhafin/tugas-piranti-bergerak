76cf7a1fa620ccd993bf33fbb07d856fc0d34ff2
Muhammad Dhafin Rizqullah <work.dhafin@gmail.com>
2025-11-09 12:09:09 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tugas-2-pbergerak',
+  webDir: 'dist'
+};
+
+export default config;
